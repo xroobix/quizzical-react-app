2bf98dd5a3bfd5af52c464897f2cb098eea1e9e9
xroobix <109462535+xroobix@users.noreply.github.com>
2022-08-06 14:56:54 +0200

    map data and display questions

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,7 +1,10 @@
-export default function Question(){
+export default function Question(props){
+
+
+    
     return(
         <div className="question">
-
+            <h2>{props.data.question}</h2>
         </div>
     )
 }
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,14 +1,22 @@
 import Question from './Question'
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
+import { nanoid } from 'nanoid'
 export default function Quiz(props){
 
     const [data, setData] = useState(() => props.data)
 
+    const questions = data.map(question => (
+        <Question 
+            key={nanoid()}
+            data={question}
+        />
+    ))
     
 
     return(
         <div className="quiz">
-            
+            {questions}
+            <button>Check answers</button>
         </div>
     )
 }
